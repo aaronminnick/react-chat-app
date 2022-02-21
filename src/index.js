fab78fa4d050ad57b864ed67508c1c0a27b32f49
Aaron Minnick <abminnick@gmail.com>
2022-02-21 11:50:43 -0800

    configure for firebase
    Co-authored-by: Skylar Brockbank <skylar.brockbank@gmail.com>

--- src/index.js
+++ src/index.js
@@ -5,10 +5,31 @@ import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
+import {createStore} from 'redux';
+// import reducer from 'WhereverTheReducerComesFromEventually';
+import {Provider} from 'react-redux';
+
+import {ReactReduxFirebaseProvider} from 'react-redux-firebase';
+import {createFirestoreInstance} from 'redux-firestore';
+import firebase from "./firebase";
+
+const store = createStore(reducer);
+
+const rrfProps = {
+  firebase,
+  config:{
+    userProfile: 'users'
+  },
+  dispatch: store.dispatch,
+  createFirestoreInstance
+}
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store = {store}>
+    <ReactReduxFirebaseProvider {...rrfProps}>
+      <App />
+    </ReactReduxFirebaseProvider>
+  </Provider>,
   document.getElementById('root')
 );
 
