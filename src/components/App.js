9ae8489f792c0d1384689eebac82137ca629b1e8
Aaron Minnick <abminnick@gmail.com>
2022-02-22 10:46:22 -0800

    make App correctly create a default channel in firebase if there are no channels in firebase
    Co-authored-by: Skylar Brockbank <skylar.brockbank@gmail.com>

--- src/components/App.js
+++ src/components/App.js
@@ -4,8 +4,27 @@ import ButtonPane from './ButtonPane';
 import ChatPane from './ChatPane';
 import UserList from './UserList';
 import Col from 'react-bootstrap/Col';
+import {useFirestoreConnect, isEmpty, useFirestore, useFirebase} from 'react-redux-firebase';
+import {useSelector, connect} from 'react-redux';
 
-function App() {
+function App(props) {
+  useFirestoreConnect({collection: "channels"});
+  const firestore = useFirestore();
+  const {dispatch} = props;
+  // make a default channel if there are no channels
+  firestore.collection("channels").get().then(channels => {
+    if (channels._delegate._snapshot.docChanges.length == 0) {
+      let action = {type: "SET_CURRENT_CHANNEL"};
+      firestore.collection("channels").add({
+        name: "General Discussion"
+      })
+      .then(docRef => {
+        action = {...action, currentChannelId: docRef.id}
+        dispatch(action);
+      });
+    }
+  });
+
   return (
     <React.Fragment>
       <Col className="col-3">
@@ -21,5 +40,7 @@ function App() {
     </React.Fragment>
   );
 }
+
+App = connect()(App);
 
 export default App;
