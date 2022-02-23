d9f9e7c6ce8605dfa145354c7797a3529f6aef12
Aaron Minnick <abminnick@gmail.com>
2022-02-22 16:22:10 -0800

    add word wrap to messages
    Co-authored-by: Skylar Brockbank <skylar.brockbank@gmail.com>

--- src/components/MessageItem.js
+++ src/components/MessageItem.js
@@ -3,8 +3,8 @@ import React from 'react';
 function MessageItem(props) {
   return (
     <React.Fragment>
-      <div>
-        <p>{props.message.content}</p>
+      <div >
+        <p style={{wordBreak: "break-all"}}>{props.message.content}</p>
       <img src={props.message.avatarImg} alt={`so and so's avatar`} />
       </div>
     </React.Fragment>
